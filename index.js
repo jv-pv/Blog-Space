f3f96c5f5e256720eb7f856b807f41708168b3a5
John Pieri <jvpieri@gmail.com>
2023-07-15 14:30:58 -0400

    Added API text placeholder logic

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ fetch("https://apis.scrimba.com/jsonplaceholder/posts")
   .then((data) => {
     const postsArr = data.slice(0, 5);
     postsArr.forEach((post) => {
-        const postCont = document.createElement('div')
-        const postTitle = document.createElement('h1')
-        const postBody = document.createElement('p')
-        postCont.className = "post-container"
-        postTitle.innerText = post.title
-        postBody.innerText = post.body
-        postCont.append(postTitle, postBody)
-        document.querySelector('main').appendChild(postCont)
-        document.querySelector('main').className = "main-container"
+      const postCont = document.createElement("div");
+      const postTitle = document.createElement("h1");
+      const postBody = document.createElement("p");
+      postCont.className = "post-container";
+      postTitle.innerText = post.title;
+      postBody.innerText = post.body;
+      postCont.append(postTitle, postBody);
+      document.querySelector("main").appendChild(postCont);
+      document.querySelector("main").className = "main-container";
     });
   });
