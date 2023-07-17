3d1058a6483263a3d6a06be9d487d609db62a153
John Pieri <jvpieri@gmail.com>
2023-07-17 18:38:23 -0400

    Refactoed redundant code into render, & clear form

--- index.js
+++ index.js
@@ -1,48 +1,59 @@
-let postArray = []
+let postArray = [];
+const form = document.querySelector('form')
+const postTitle = document.getElementById("post-title");
+const postBody = document.getElementById("post-body");
 
-function renderPosts(){
-    let html = ""
-    postArray.forEach(post => {
-        html += `
+function renderPosts() {
+  let html = "";
+  postArray.forEach((post) => {
+    html += `
         <div class="post-container">
             <h1>${post.title}</h1>
             <p>${post.body}</p>
         </div>
-        `
-    })
-    document.querySelector('.post-wrapper').innerHTML = html;
+        `;
+  });
+  document.querySelector(".post-wrapper").innerHTML = html;
 }
 
 fetch("https://apis.scrimba.com/jsonplaceholder/posts")
-.then(response => response.json())
-.then(data => {
-    postArray = data.slice(0, 5)
-    renderPosts()
-})
+  .then((response) => response.json())
+  .then((data) => {
+    postArray = data.slice(0, 5);
+    renderPosts();
+  });
 
-document.querySelector("form").addEventListener("submit", (e) => {
-    e.preventDefault();
-    const postTitleValue = document.getElementById("post-title").value;
-    const postBodyValue = document.getElementById("post-body").value;
-    let newPost = {
-        title: postTitleValue,
-        body: postBodyValue,
-    };
-    const options = {
-        method: "POST",
-        body: JSON.stringify(newPost),
-        headers: {
-            "Content-Type": "application/json",
-        },
-    };
-    fetch("https://apis.scrimba.com/jsonplaceholder/posts", options)
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  let newPost = {
+    title: postTitle.value,
+    body: postBody.value,
+  };
+  const options = {
+    method: "POST",
+    body: JSON.stringify(newPost),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
+  fetch("https://apis.scrimba.com/jsonplaceholder/posts", options)
     .then((response) => response.json())
     .then((postData) => {
-        postArray.unshift(postData)
-        renderPosts()
+      if (postTitle.value !== "" || postBody.value !== "") {
+        postArray.unshift(postData);
+        // Two ways to clear form. One uses a function, form.reset() uses a local method
+        // clearForm(postTitle, postBody);
+        form.reset()
+        renderPosts();
+      }
     });
 });
 
+function clearForm(title, body) {
+  title.value = "";
+  body.value = "";
+}
 
 // Alternate DOM Manipulation
 
@@ -81,8 +92,6 @@ document.querySelector("form").addEventListener("submit", (e) => {
 //     .then((response) => response.json())
 //     .then((postData) => console.log(postData));
 // });
-
-
 
 // document.querySelector("form").addEventListener("submit", (e) => {
 //   e.preventDefault();
